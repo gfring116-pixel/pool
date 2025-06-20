d061c1c0691ac30a9b55c2d4a46e6446e0b7d56e
IdkotsValkk <165882258+IdkotsValkk@users.noreply.github.com>
2025-06-20 13:31:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import logging
 from dotenv import load_dotenv
 import os
 
-load_dotenv() token = os.getenv('DISCORD_TOKEN')
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w') intents = discord.Intents.default() intents.message_content = True intents.members = True
 
